420abd16abe6df3c7e34e71565224ed5cd9123ef
denny <denycam9@gmail.com>
2026-04-30 09:24:41 +0700

    feat: Add ProductCreateDto and implement GetProductById and CreateProduct methods in ProductController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Inventra.Data;
+using Inventra.DTOs.Product;
 using Inventra.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -20,5 +21,34 @@ public class ProductController : ControllerBase
     public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
     {
         return await _context.Products.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProductById(Guid id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(ProductCreateDto product)
+    {
+        var newProduct = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = product.Name,
+            Sku = product.Sku,
+            PurchasePrice = product.PurchasePrice,
+            SellingPrice = product.SellingPrice,
+            Stock = product.Stock
+        };
+
+        _context.Products.Add(newProduct);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetProductById), new { id = newProduct.Id }, newProduct);
     }
 }
